041d6e5130d618c59465d60b1b3e2e410467dd52
Evgeni Georgiev <evgenigg2000@gmail.com>
2022-08-31 18:13:18 +0300

    Set new route restrictions.

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,11 @@ use App\Http\Controllers\CKEditorController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\Subscriptions\PaymentController;
 use App\Http\Controllers\Subscriptions\SubscriptionController;
 use App\Http\Middleware\CheckIfPaied;
-use App\Models\User;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,39 +24,39 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PostController::class, 'index']);
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
-Route::get('/category/{category}', [PostController::class, 'category']);
-Route::get('/user/{user}', [PostController::class, 'user']);
+Route::get('/posts', [PostController::class, 'index'])->middleware(['auth','verified']);
+Route::get('/posts/{post}', [PostController::class, 'show'])->middleware(['auth','verified']);
+Route::get('/category/{category}', [PostController::class, 'category'])->middleware(['auth','verified']);
+Route::get('/user/{user}', [PostController::class, 'user'])->middleware(['auth','verified']);
 
 
 Route::group(['namespace' => 'Subscriptions'], function() {
-    Route::get('plans', [SubscriptionController::class, 'index'])->name('plans');
-    Route::get('/payments', [PaymentController::class, 'index'])->name('payments');
-    Route::post('/payments', [PaymentController::class, 'store'])->name('payments.store');
+    Route::get('plans', [SubscriptionController::class, 'index'])->name('plans')->middleware(['auth','verified']);
+    Route::get('/payments', [PaymentController::class, 'index'])->name('payments')->middleware(['auth','verified']);
+    Route::post('/payments', [PaymentController::class, 'store'])->name('payments.store')->middleware(['auth','verified']);
 });
 
 
-Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('edit')->middleware('auth');
+Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('edit')->middleware(['auth','verified']);
 
 Route::post('newsletter', NewsletterController::class);
-Route::post('ckeditor/image_upload', [CKEditorController::class, 'store'])->name('ckeditor.upload');
+Route::post('ckeditor/image_upload', [CKEditorController::class, 'store'])->name('ckeditor.upload')->middleware(['auth','verified']);
 
 Route::middleware([CheckIfPaied::class])->group(function() {
     Route::get('post/create', [PostController::class, 'create'])->middleware('auth');
 });
 
-Route::post('post', [PostController::class, 'store'])->name('post.store')->middleware('auth');
+Route::post('post', [PostController::class, 'store'])->name('post.store')->middleware(['auth','verified']);
 
 Route::put('posts/{post}', [PostController::class, 'update'])->name('updatePost');
 
 Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('post.destroy');
 
-Route::get('profile/{user}/edit', [SessionsController::class, 'profile_page'])->middleware('auth');
-Route::put('profile/{user}/edit', [SessionsController::class, 'update'])->name('profile.update')->middleware('auth');
+Route::get('profile/{user}/edit', [SessionsController::class, 'profile_page'])->middleware(['auth','verified']);
+Route::put('profile/{user}/edit', [SessionsController::class, 'update'])->name('profile.update')->middleware(['auth','verified']);
 
-Route::get('change/{user}/password',  [ChangePasswordController::class,'getPageChangePassword'])->middleware('auth');
-Route::post('change/{user}/password',  [ChangePasswordController::class,'changePassword'])->name('profile.change.password')->middleware('auth');
+//Route::get('change/{user}/password',  [ChangePasswordController::class,'getPageChangePassword'])->middleware(['auth','verified']);
+//Route::post('change/{user}/password',  [ChangePasswordController::class,'changePassword'])->name('profile.change.password')->middleware(['auth','verified']);
 
 Route::post('comments', [CommentController::class, 'store'])->name('comments.store');
 
