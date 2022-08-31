71153f5b3814cf09c2c6a0d64a7ac47a1aa2d6a8
Evgeni Georgiev <evgenigg2000@gmail.com>
2022-08-31 18:05:17 +0300

    Remove restrictions from routes.

--- routes/auth.php
+++ routes/auth.php
@@ -33,11 +33,11 @@ Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
                 ->name('password.email');
 
 Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
-                ->middleware('guest')
+//                ->middleware('auth')
                 ->name('password.reset');
 
 Route::post('/reset-password', [NewPasswordController::class, 'store'])
-                ->middleware('guest')
+//                ->middleware('auth')
                 ->name('password.update');
 
 Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
