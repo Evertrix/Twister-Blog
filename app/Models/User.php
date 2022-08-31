194e3d643c20803d30af0501b2adb4052eac844d
Evgeni Georgiev <evgenigg2000@gmail.com>
2022-08-31 18:10:57 +0300

    Implemented user email verification when registered.

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 use Stripe\Plan;
 use Stripe\Subscription;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, Billable;
 
